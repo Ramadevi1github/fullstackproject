40e72354c6cb40fcaa8ca80df3a019216236feef
Ramadevi1github <119603968+Ramadevi1github@users.noreply.github.com>
2022-12-23 15:23:25 +0530

    Add files via upload

---
+++ Administrator.java
@@ -0,0 +1,18 @@
+package com.project.fullstack1.blueprint;
+
+public class Administrator {
+private String UserName;
+private String Password;
+public String getUserName() {
+	return UserName;
+}
+public void setUserName(String Username) {
+	this.UserName = Username;
+}
+public String getPassword() {
+	return Password;
+}
+public void setPassword(String password) {
+	this.Password = password;
+}
+}
